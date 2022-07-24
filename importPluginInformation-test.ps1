8bf7faa36d1d8c526c83565764d2621472091f0f
Jose Partida <josepartida.rh@gmail.com>
2022-07-24 12:18:14 -0700

    Plugin information gathering test

---
+++ importPluginInformation-test.ps1
@@ -0,0 +1,36 @@
+$pluginRepositoryPath = "c:\l4d2-serverbuilder\repository"
+$pluginFolderList = Get-ChildItem -Path $pluginRepositoryPath
+
+$allAvailablePlugins = @{}
+$buildinfoSuccess = @{}
+
+foreach ($pluginFolder in $pluginFolderList) {
+	$builderFolderLocation = $pluginFolder.FullName + "\builderinfo.csv"
+	if(Test-Path $builderFolderLocation -PathType Leaf){
+
+		write-host "Found" $pluginFolder.Name "with build file." -ForegroundColor DarkGreen
+
+		#Importing Plugin Build File CSV
+		$pluginRawBuildData = import-csv -path $builderFolderLocation
+
+		#Converting array into usable hashtable and adding into global plugins hashtable
+		$BuildDataHashtable = @{}
+
+		foreach ($RawBuildData in $pluginRawBuildData){
+
+			$BuildDataHashtable.Add($RawBuildData.Key,$RawBuildData.Value)
+
+		}
+
+		#Adding hashtable into global plugins list
+		$allAvailablePlugins.Add($BuildDataHashtable.name,$BuildDataHashtable)
+		write-host "Added" $BuildDataHashtable.name "into plugin table."
+
+	}
+	else
+	{
+
+		write-host "[ERROR]" $pluginFolder.Name "is missing a build file" -ForegroundColor Red
+
+	}
+}
